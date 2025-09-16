450c1076636761fa029d9d990d083b7fe3c42bda
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2025-09-16 22:07:19 +0530

    Wrong code pulled issue resolved

--- UserMicroService/src/main/java/com/StarkIndustries/UserMicroService/controller/UserController.java
+++ UserMicroService/src/main/java/com/StarkIndustries/UserMicroService/controller/UserController.java
@@ -5,12 +5,10 @@ import com.StarkIndustries.UserMicroService.model.User;
 import com.StarkIndustries.UserMicroService.service.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-<<<<<<< HEAD
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-=======
->>>>>>> 56c7ead147886cae4b67f5426d1e0fda622b3fb8
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,10 +26,8 @@ import java.util.Objects;
 @RequestMapping("/user")
 public class UserController {
 
-<<<<<<< HEAD
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
-=======
->>>>>>> 56c7ead147886cae4b67f5426d1e0fda622b3fb8
+
     @Autowired
     public UserService userService;
 
@@ -40,11 +36,8 @@ public class UserController {
         return ResponseEntity.ok("Greetings,I am Optimus Prime!!");
     }
 
-<<<<<<< HEAD
     public int count=0;
 
-=======
->>>>>>> 56c7ead147886cae4b67f5426d1e0fda622b3fb8
     @PostMapping("/add-user")
     public ResponseEntity<?> addUser(@RequestBody User user){
 
@@ -54,24 +47,17 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(user1);
     }
 
+
     @GetMapping("/get-users")
-<<<<<<< HEAD
-//    @CircuitBreaker(name = "getAllUsersBreaker",fallbackMethod = "getAllUsersFallBack")
+    @CircuitBreaker(name = "getAllUsersBreaker",fallbackMethod = "getAllUsersFallBack")
     @Retry(name = "getAllUsersRetry",fallbackMethod = "getAllUsersFallBack")
     public ResponseEntity<?> getUsers(){
-        log.info("count:{}",count++);
-=======
-    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "hotelAndRatingBreakerFallback")
-    @Retry(name = "ratingHotelRetry",fallbackMethod = "")
-    public ResponseEntity<?> getUsers(){
->>>>>>> 56c7ead147886cae4b67f5426d1e0fda622b3fb8
         List<User> userList = this.userService.getUsers();
         if(userList.isEmpty())
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Enter users first!!");
         return ResponseEntity.status(HttpStatus.OK).body(userList);
     }
 
-<<<<<<< HEAD
     public ResponseEntity<?> getAllUsersFallBack(Exception exception){
 
         Map<String, Object> response = new HashMap<>();
@@ -87,20 +73,12 @@ public class UserController {
 
     }
 
+
     @GetMapping("/get-user-by-id/{userId}")
-//    @CircuitBreaker(name = "getUserByIdBreaker",fallbackMethod = "getUserByIdFallback")
-    @Retry(name = "getUserByIdRetry",fallbackMethod = "getUserByIdFallback")
+    @CircuitBreaker(name = "getUserByIdBreaker",fallbackMethod = "getUsersByIdFallBack")
+    @Retry(name = "getUserByIdRetry",fallbackMethod = "getUsersByIdFallBack")
     public ResponseEntity<?> getUserById(@PathVariable("userId") String userId){
 
-        log.info("count:{}",count++);
-
-=======
-    @GetMapping("/get-user-by-id/{userId}")
-    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "hotelAndRatingFallback")
-    @Retry(name = "ratingHotelRetry",fallbackMethod = "")
-    public ResponseEntity<?> getUserById(@PathVariable("userId") String userId){
-
->>>>>>> 56c7ead147886cae4b67f5426d1e0fda622b3fb8
         User user = this.userService.getUser(userId);
         if(user!=null)
             return ResponseEntity.status(HttpStatus.OK).body(user);
@@ -108,13 +86,9 @@ public class UserController {
 
     }
 
-<<<<<<< HEAD
 
-    public ResponseEntity<?> getUserByIdFallback(String userId,Exception exception){
-=======
-    public ResponseEntity<?> hotelAndRatingBreakerFallback(Exception exception){
+    public ResponseEntity<?> getUsersByIdFallBack(Exception exception){
 
->>>>>>> 56c7ead147886cae4b67f5426d1e0fda622b3fb8
         Map<String, Object> response = new HashMap<>();
 
         log.info("Message:Service down!!,{}",exception.getLocalizedMessage());
@@ -125,15 +99,11 @@ public class UserController {
         response.put(Keys.MESSAGE,"Service down!!");
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
-<<<<<<< HEAD
-    }
-
-=======
-
     }
 
 
->>>>>>> 56c7ead147886cae4b67f5426d1e0fda622b3fb8
+
+
     @PutMapping("/update-user/{userId}")
     public ResponseEntity<?> updateUser(@PathVariable("userId") String userId,@RequestBody User user){
 
